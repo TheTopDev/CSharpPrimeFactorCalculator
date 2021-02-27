0241cda7a3f3b13d933104f37bb7e07fb930663d
Lucas Garofalo <garofalodev@gmail.com>
2021-02-26 23:50:01 -0600

    First commit

---
+++ Mod2P1/PrimeFactors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Mod2P1
+{
+    public class PrimeFactors
+    {
+        static void PrimeFactorsCalculator(int num)
+        {
+            while (num % 2 == 0) {
+                Console.Write(2 + "X");
+                num /= 2;
+            }
+
+            for (int i = 3; i <= Math.Sqrt(num); i += 2){
+                while (num % i == 0) {
+                    Console.Write(i + "X");
+                    num /= i;
+                }
+            }
+            if (num > 2) 
+                Console.Write(num);
+        }
+
+        public static void Main() {
+
+            Console.WriteLine("Please enter a number to find its prime factors or enter ");
+                try  {
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    PrimeFactorsCalculator(number);
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Please check your input" + ex.Message);
+                }
+        }
+    }
+}
---
+++ Mod2P1Tests/PrimeFactorsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mod2P1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod2P1.Tests
+{
+    [TestClass()]
+    public class PrimeFactorsTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
